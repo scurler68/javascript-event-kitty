99f0b56346974ce2987e2fa8e3147e2bf4b3a994
sharon_d <sjdohrmann@gmail.com>
2023-09-12 13:13:33 -0700

    added if else statements to change button behavior

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.classList.add("disappear");
+    //   button.innerText = "Wait, come back!";
+  } else {
     cat.classList.add("show");
-})
+    button.classList.remove("disappear");
+    //    button.innerText = "Shoot, cat!";
+  }
+});
